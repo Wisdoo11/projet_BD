7df0f7ec3d6a3125157806b4aa55a6a11bd86887
Sébastien Herbreteau <sebastien.herbreteau@grenoble-inp.org>
2018-11-28 17:07:22 +0100

    Le projet compile de nouveau

--- src/affichage/AjouterUneSalle.java
+++ src/affichage/AjouterUneSalle.java
@@ -15,11 +15,11 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class AjouterUneSalle extends JDialog {
-	
+
 	private JButton bouton1 = new JButton("ajouter une salle");
-	
+
 	public AjouterUneSalle() {
-		this.setTitle("Beinvenue");
+		this.setTitle("Bienvenue");
 		this.setSize(750, 300);
 		this.setResizable(false);
 		this.setLocationRelativeTo(null);
@@ -28,23 +28,23 @@ public class AjouterUneSalle extends JDialog {
 		// texte de bienvenue
 		JPanel panel = new JPanel();
 		panel.setLayout(new FlowLayout());
-			   
+
 		// Texte de Bienvenue
 		String labelText = "<html><P ALIGN=CENTER STYLE=\"margin-bottom: 0in\"><BR>Que souhaitez-vous faire ?</P></HTML>";
 		JLabel label = new JLabel(labelText);
 		label.setLayout(new BoxLayout(label, BoxLayout.LINE_AXIS));
 		panel.add(label);
 		setContentPane(panel);
-			    
+
 		// Choisir entre utilisateur et administrateur
 		Box b2 = Box.createHorizontalBox();
 		b2.add(bouton1);
-			    
+
 		Box b4 = Box.createVerticalBox();
 		b4.add(b2);
 
 		this.getContentPane().add(b4);
-			    
+
 		bouton1.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent arg0) {
 				Fenetre fenetre = new Fenetre();
